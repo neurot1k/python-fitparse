71847c8e80244cd44573d3ac4fca94f4e17b8e4e
Mike Smith <sendmikestuff@gmail.com>
2016-09-17 07:56:20 -0700

    Update StandardUnitsDataProcessor

--- fitparse/processors.py
+++ fitparse/processors.py
@@ -7,35 +7,46 @@ class FitFileDataProcessor(object):
     #def process_field_<field_name> (field_data) -- can be unknown_DD but NOT recommended
     #def process_message_<mesg_name / mesg_type_num> (data_message)
 
-    def process_type_bool(self, field_data):
+    @staticmethod
+    def process_type_bool(field_data):
         if field_data.value is not None:
             field_data.value = bool(field_data.value)
 
-    def process_type_date_time(self, field_data):
+    @staticmethod
+    def process_type_date_time(field_data):
         value = field_data.value
         if value is not None and value >= 0x10000000:
             field_data.value = datetime.datetime.utcfromtimestamp(631065600 + value)
             field_data.units = None  # Units were 's', set to None
 
-    def process_type_local_date_time(self, field_data):
+    @staticmethod
+    def process_type_local_date_time(field_data):
         if field_data.value is not None:
             field_data.value = datetime.datetime.fromtimestamp(631065600 + field_data.value)
             field_data.units = None
 
 
 class StandardUnitsDataProcessor(FitFileDataProcessor):
-    # Example use case
-    def process_field_distance(self, field_data):
+    @staticmethod
+    def process_field_distance(field_data):
         if field_data.value is not None:
             field_data.value /= 1000.0
         field_data.units = 'km'
 
-    def process_field_speed(self, field_data):
+    @staticmethod
+    def process_field_speed(field_data):
         if field_data.value is not None:
-            field_data.value *= 60.0 * 60.0 / 1000.0
-        field_data.units = 'km/h'
+            field_data.value /= 1000.0
+        field_data.units = 'm/s'
 
-    def process_units_semicircles(self, field_data):
+    @staticmethod
+    def process_field_altitude(field_data):
+        if field_data.value is not None:
+            field_data.value = (field_data.value / 5.0) - 500
+        field_data.units = 'm'
+
+    @staticmethod
+    def process_units_semicircles(field_data):
         if field_data.value is not None:
             field_data.value *= 180.0 / (2 ** 31)
         field_data.units = 'deg'
